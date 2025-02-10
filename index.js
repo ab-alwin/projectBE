06e4e40e8b6caa83b908b74a5180d78766bb96e0
ab-alwin <abrahamalwin99@gmail.com>
2025-02-10 13:24:47 -0500

    port fix

--- index.js
+++ index.js
@@ -61,6 +61,6 @@ app.delete('/contacts/:id', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
+  });
